a8b5ac1f463c742b958c511d402d7947afb810e7
HayarovEd <edurd@rambler.ru>
2021-11-30 10:55:49 +0500

    add menu and noteActivity

---
+++ app/src/main/java/com/edurda77/Blocknote2021/NoteActivity.kt
@@ -0,0 +1,11 @@
+package com.edurda77.Blocknote2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
