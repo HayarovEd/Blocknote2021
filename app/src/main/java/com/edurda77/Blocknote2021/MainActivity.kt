ce6d4e61d10697f8a351ff6b938ba64124247481
HayarovEd <edurd@rambler.ru>
2021-11-30 11:09:43 +0500

    add toolbar and binding in all activities

--- app/src/main/java/com/edurda77/Blocknote2021/MainActivity.kt
+++ app/src/main/java/com/edurda77/Blocknote2021/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.edurda77.Blocknote2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.edurda77.Blocknote2021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.new_note -> {
+                val intent = Intent(this, NoteActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.custom -> {
+                val intent = Intent(this, CustomActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/edurda77/Blocknote2021/NoteActivity.kt
+++ app/src/main/java/com/edurda77/Blocknote2021/NoteActivity.kt
@@ -1,11 +1,42 @@
 package com.edurda77.Blocknote2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.edurda77.Blocknote2021.databinding.ActivityMainBinding
+import com.edurda77.Blocknote2021.databinding.ActivityNoteBinding
 
 class NoteActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    private lateinit var binding: ActivityNoteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityNoteBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note)
+        setContentView(binding.root)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.new_note -> {
+                val intent = Intent(this, NoteActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.custom -> {
+                val intent = Intent(this, CustomActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
